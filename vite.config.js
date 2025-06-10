0ef44dac62a09685f9c35ea412f9b4e9004dcb76
Cadavid06 <carlos.ricardoreiino@gmail.com>
2025-06-10 10:46:59 -0500

    configuracion ruta de inicio

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Restaurante-Alva-rril/", // Añade esta línea con el nombre de tu repositorio
+  base: "/Asados-Alva-rril/", 
 })
