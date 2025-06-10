eef46ec28f2f22a2b7d653e9a513aee957e6d591
Cadavid06 <carlos.ricardoreiino@gmail.com>
2025-06-10 10:38:22 -0500

    configuracion vite

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-    sourcemap: false,
-  },
-  assetsInclude: ["**/*.png", "**/*.jpg", "**/*.jpeg", "**/*.gif", "**/*.svg"],
+  base: "/Restaurante-Alva-rril/", // Añade esta línea con el nombre de tu repositorio
 })
